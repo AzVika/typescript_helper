583c3e754599eb1f4a951cd597bdb22f99d3a8c9
AzVika <26838569+AzVika@users.noreply.github.com>
2025-01-11 11:40:01 +0200

    add typescript for function

--- typescript_helper.ts
+++ typescript_helper.ts
@@ -61,3 +61,71 @@ if (a10 < 10) {
 
 
 
+// Function /////////////////////////
+
+function consolePrinter(str: string): void {
+	console.log(str);
+}
+consolePrinter("Welcome");
+
+
+const infoUser = (name: string, age: number): void => {
+	console.log(name, ", ", age);
+}
+infoUser("John", 22);
+
+
+function sum(num1: number, num2: number): number {
+	return num1 + num2;
+}
+sum(4, 6);
+
+
+const multy = (num1: number, num2: number): string => {
+	return `${num1} * ${num2} = ${num1 * num2}`;
+}
+multy(2, 6);
+
+
+const anyFun = (arg: any): any => { return arg }; // bad practic
+console.log(anyFun("Hi"));
+console.log(anyFun({ name: "Max" }));
+
+
+function infoCat(name: string, age?: number): void {
+	if (age) {
+		console.log('Cat ', name, ", ", age);
+	} else {
+		console.log('Cat ', name);
+	}
+}
+infoCat("Kikky", 3);
+infoCat("Clever");
+
+
+const underfinedFun = (count: number | null): void {
+	console.log(count);
+}
+underfinedFun(4);
+underfinedFun(null);
+
+
+const updateUser = (id: number, name: string = "Guest", active: boolean = true): void => {
+	console.log(id, ` Hello ${name}! `, active);
+}
+updateUser(3);
+updateUser(5, "Max");
+updateUser(10, "John", false);
+
+
+const numbersToSting = (...arg: number[]): void => {
+	console.log(arg.join(', '));
+}
+numbersToSting(4, 12, 14, 18);
+
+const setName = (name: string, ...arg: any): void => {
+	console.log(name);
+}
+setName("Max", 3, true);
+setName("John", 4);
+setName("Sully");
